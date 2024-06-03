d7b8d099a75c2701f2713f1547434ab57c4a0038
xin <102584033+golden-assassin@users.noreply.github.com>
2024-06-03 10:28:19 +0900

    Update file.js

--- script/file.js
+++ script/file.js
@@ -1,8 +1,9 @@
 if (location.host === "www.twitch.tv") {
   (async function() {
      while (true) {
-      await new Promise(resolve => setTimeout(resolve, 10000));
+      await new Promise(resolve => setTimeout(resolve, 1000));
       const sunege = document.querySelector('[aria-label="ボーナスを受け取る"]');
+      console.log("すね毛",sunege)
       if (sunege && window.getComputedStyle(sunege).display !== 'none') {
         sunege.click();
       }
