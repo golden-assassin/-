5e6bc60b34a3a25d7e9bf6b09634cf0e8b4d7ad4
xin <102584033+golden-assassin@users.noreply.github.com>
2024-06-06 17:41:20 +0900

    Update and rename file.js to anal.js

--- script/anal.js
+++ script/anal.js
@@ -1,9 +1,9 @@
 if (location.host === "www.twitch.tv") {
   (async function() {
      while (true) {
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise(resolve => setTimeout(resolve, 1500));
       const sunege = document.querySelector('[aria-label="ボーナスを受け取る"]');
-      console.log("すね毛",sunege)
+      console.log("すね毛 v2",sunege)
       if (sunege && window.getComputedStyle(sunege).display !== 'none') {
         sunege.click();
         console.log("回収")
